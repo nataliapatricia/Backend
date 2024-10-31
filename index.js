b83c25b7e32128395e79d49e6df5e62f283f9831
Natalia Souza <nataliapatriciadesouza@pm.me>
2024-10-31 10:04:56 -0300

    ponto-ads

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let app= express();
+
+const port = 3000;
+
+//app.METHOO(path, callback [, callback...])  
+app.get("\teste", (req, res) => {
+res.send("Resposta da rota /teste");
+});   
+
+
+app.listen(port,() =>{
+    console.log(`servidor escutando a porta ${port}`);
+})
